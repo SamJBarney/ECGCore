68c951c9cd053007a8306b488ed1eda5c60fee8d
Samuel Barney <sbarney@instructure.com>
2016-06-15 11:41:35 -0600

    Added some documentation

--- src/ecg.h
+++ src/ecg.h
@@ -1,7 +1,26 @@
 #pragma once
 
+/***
+* Creates the ticking threads
+*/
 void ecg_init();
+
+/***
+* Preps the ticking threads for ticking
+*/
 void ecg_start();
+
+/***
+* Ticks the threads
+*/
 bool ecg_tick();
+
+/***
+* Checks whether there is a current tick going on
+*/
 bool ecg_ticking();
+
+/***
+* Stops the execution of the ticking threads
+*/
 void ecg_cleanup();
--- src/entity.h
+++ src/entity.h
@@ -11,8 +11,27 @@
 
 typedef uint32_t entity_t;
 
+/***
+* Sets up the entity system
+*/
 void entity_init(uint32_t a_Mask);
+
+/***
+* Creates a new entity
+*/
 API_VISIBLE entity_t entity_new(uint32_t);
+
+/***
+* Checks whether an entity is valid
+*/
 API_VISIBLE bool entity_valid(entity_t);
+
+/***
+* Checks if the entity exists within the system
+*/
 API_VISIBLE bool entity_exists(entity_t);
+
+/***
+* Removes an entity from the system
+*/
 API_VISIBLE void entity_delete(entity_t);
